8d5dee36bdd942f57d20328ae87b91067dc935c1
Andrew Walter <awalter@bnl.gov>
2024-07-25 09:18:13 -0400

    Updating the PlanCollector and PlanCollectorSub to use types.SimpleNamespace

--- src/ari_sxn_common/bluesky_test_setup.py
+++ src/ari_sxn_common/bluesky_test_setup.py
@@ -2,8 +2,8 @@
 import ari_ophyd
 from bluesky import RunEngine
 from bluesky.callbacks.best_effort import BestEffortCallback
-from bluesky import plans as plans
-from bluesky import plan_stubs as plan_stubs
+# from bluesky import plans as plans
+# from bluesky import plan_stubs as plan_stubs
 from bluesky.utils import ProgressBarManager
 import common_bluesky
 from databroker import Broker
@@ -23,13 +23,13 @@ RE.waiting_hook = ProgressBarManager()
 EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)
 
 # create the plan_stub objects
-for plan_stub, aliases in common_bluesky._plan_stubs_to_import.items():
+for aliases, plan_stub in common_bluesky._plan_stubs_to_alias.items():
     for alias in aliases:
-        globals()[alias] = getattr(plan_stubs, plan_stub)
+        globals()[alias] = plan_stub
 # create the plan objects
-for plan, aliases in common_bluesky._plans_to_import.items():
+for aliases, plan in common_bluesky._plans_to_alias.items():
     for alias in aliases:
-        globals()[alias] = getattr(plans, plan)
+        globals()[alias] = plan
 
 # Setup the m1 mirror ophyd object
 m1_locations_data = {'measure': {'diag.locations': ('Out', None),
@@ -40,7 +40,7 @@ m1 = ari_ophyd.M1('ARI_M1:', name='m1', locations_data=m1_locations_data,
                   labels=('device',))
 M1 = m1  # Create a reference object so that m1 or M1 are equivalent.
 
+common_bluesky._plans_to_alias.update(common_bluesky._plan_stubs_to_alias)
 plans = common_bluesky.PlanCollector(
-    plans_to_import=common_bluesky._plans_to_import,
-    plan_stubs_to_import=common_bluesky._plan_stubs_to_import,
+    plans_for_methods=common_bluesky._plans_to_alias,
     name='plans')
